9d69e87651f04f589d8773446bc07456c0a8305a
NickBurggraaff <Nick.Burggraaff@gmail.com>
2018-10-16 21:22:57 +0200

    Initial after VR_Week_1 repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+//public class PlayerController : NetworkBehaviour
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed = 1f;
+    public Camera FPCamera;
+    public float Camera_Sensitivity;
+    public float Camera_Smoothing;
+    public Vector2 SmoothV;
+    public Vector2 MouseLook;
+
+    public bool IsStearing;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        BodyRotation();
+    }
+
+    public void Movement()
+    {
+        float Translation_X = Input.GetAxis("Horizontal") * Speed;
+        float Translation_Z = Input.GetAxis("Vertical") * Speed;
+        Translation_X *= Time.deltaTime;
+        Translation_Z *= Time.deltaTime;
+        transform.TransformDirection(Vector3.forward);
+        transform.Translate(new Vector3(Translation_X, 0, Translation_Z));
+    }
+
+    public void BodyRotation()
+    {
+        //var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        //md = Vector2.Scale(md,  new Vector2(Camera_Sensitivity * Camera_Smoothing, Camera_Sensitivity * Camera_Smoothing));
+        //SmoothV.x = Mathf.Lerp(SmoothV.x, md.x, 1f / Camera_Smoothing);
+        //SmoothV.y = Mathf.Lerp(SmoothV.y, md.y, 1f / Camera_Smoothing);
+        //MouseLook += SmoothV;
+
+        //FPCamera.transform.localRotation = Quaternion.AngleAxis(-MouseLook.y, Vector3.right);
+        //transform.localRotation = Quaternion.AngleAxis(MouseLook.x, transform.up);
+        transform.rotation = FPCamera.transform.localRotation;
+    }
+
+    public void ShipSteering()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    private GameObject PathParentObject;
+    private Transform targetPathNode;
+    private int pathNodeIndex = 0;
+
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PathParentObject = GameObject.Find("Path");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(targetPathNode == null)
+        {
+            GetNextPathNode();
+            if(targetPathNode == null)
+            {
+                pathNodeIndex = 0;
+            }
+        }
+
+        Vector3 directionVector = targetPathNode.position - this.transform.localPosition;
+        float distanceThisFrame = Speed * Time.deltaTime;
+
+        if(directionVector.magnitude <= distanceThisFrame)
+        {
+            targetPathNode = null;
+        }
+        else
+        {
+            transform.Translate(directionVector.normalized * distanceThisFrame, Space.World);
+            Quaternion rotation = Quaternion.LookRotation(directionVector, Vector3.forward);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, rotation, Time.deltaTime / 4);
+        }
+    }
+
+    void GetNextPathNode()
+    {
+        targetPathNode = PathParentObject.transform.GetChild(pathNodeIndex);
+        pathNodeIndex++;
+    }
+}
