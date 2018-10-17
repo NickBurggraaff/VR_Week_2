08ac8a66165d33b9931ff015cda9e7a4e6241d9c
Jeremy Fontijne <jeremyfontijne@hotmail.com>
2018-10-17 15:30:35 +0200

    Add laser shooting drone prefab

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject laser;
+
+    void Start()
+    {
+        ShootLaser();
+    }
+
+    private void ShootLaser() {
+        Instantiate(laser, transform.position, Quaternion.identity);
+
+        Invoke("ShootLaser", 3);
+    }
+}
