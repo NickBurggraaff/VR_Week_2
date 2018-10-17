518aa2c555302a7f20ce1493793babbbb534b510
Nick Burggraaff <Nick.Burggraaff@kpnplanet.nl>
2018-10-17 15:23:42 +0200

    Rockets in Sci-fi jet More enemy hordes

--- Assets/JetDirection.cs
+++ Assets/JetDirection.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class JetDirection : MonoBehaviour
 {
+    private float _RocketRespawn = 4;
     private GameObject playerShip;
+    private List<RocketController> RocketControllers;
 
     // Start is called before the first frame update
     void Start()
     {
         playerShip = GameObject.FindGameObjectWithTag("Ship");
+        RocketControllers = GetComponentsInChildren<RocketController>().ToList();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _RocketRespawn -= Time.deltaTime;
+        if (RocketControllers != null && _RocketRespawn < 0)
+        {
+            ShootRocket();
+            _RocketRespawn = 10;
+        }
+
+
         if (playerShip != null)
         {
             Vector3 directionVector = playerShip.transform.position - this.transform.localPosition;
@@ -25,5 +37,13 @@ public class JetDirection : MonoBehaviour
     void LateUpdate()
     {
         transform.localEulerAngles = new Vector3(-70, transform.localEulerAngles.y, 0);
+    }
+
+    void ShootRocket()
+    {
+        foreach (var rocket in RocketControllers)
+        {
+            rocket.Shoot(Random.Range(25, 60), Random.Range(0,5));
+        }
     }
 }
---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    private Vector3 StartPosition;
+    private float Speed;
+    private bool Fire;
+    private float ShootCooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootCooldown -= Time.deltaTime;
+        if (ShootCooldown < 0)
+        {
+            ShootCooldown = 0;
+            if (Fire)
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * Speed);
+            }
+        }
+    }
+
+    public void Shoot(float speed, float cooldown)
+    {
+        ShootCooldown = cooldown;
+        Speed = speed;
+        if (Fire)
+        {
+            transform.localPosition = StartPosition;
+        }
+        else
+        {
+            Fire = true;
+        }
+        
+    }
+}
